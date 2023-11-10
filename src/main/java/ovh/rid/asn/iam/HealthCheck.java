e497a5c648a4d358abda47fc68558c20f0439ab4
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-10 19:09:16 +0100

    json parsing test

--- src/main/java/ovh/rid/asn/iam/HealthCheck.java
+++ src/main/java/ovh/rid/asn/iam/HealthCheck.java
@@ -1,28 +1,34 @@
 package ovh.rid.asn.iam;
 
+import com.google.gson.Gson;
+
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HealthCheck {
     public static void main(String[] args) throws IOException {
 
         System.out.println("ASN-IAM Health Check (Java Implementation) made by Michał Kątnik");
 
-        SSHConnection cbea = new SSHConnection("192.168.1.200", 22, "rid", "d4rkV@rid");
-        System.out.println(cbea.runCommand("pwd"));
+//        SSHConnection cbea = new SSHConnection("192.168.1.200", 22, "rid", "test123456");
+//        System.out.println(cbea.runCommand("pwd"));
 
 
         // Test obsługi plików
-        FileOutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream("test.txt");
-            outputStream.write("test".getBytes());
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        } finally {
-            outputStream.close();
-        }
+//        FileOutputStream outputStream = null;
+//        try {
+//            outputStream = new FileOutputStream("test.txt");
+//            outputStream.write("test".getBytes());
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e);
+//        } finally {
+//            outputStream.close();
+//        }
 
+        // Json (Gson) parsing
+        InputStream jsonConfig = new FileInputStream("config/serverTypes.json");
 
     }
 }
